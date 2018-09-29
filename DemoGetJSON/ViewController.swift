b09f9ef6a104de8c89783e7fe4dcf6ca355f0456
masterUNG <phrombutr@gmail.com>
2018-09-30 05:09:16 +0700

    update

--- DemoGetJSON/ViewController.swift
+++ DemoGetJSON/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//   let urlJSoN: String = "https://androidthai.in.th/sun/getAllUser.php"
+    let urlJSoN: String = "http://androidthai.in.th/sun/getAllUser.php"
+  //  let urlJSoN: String = "https://pantip.com/topic/37337189"
+    
+    
+    @IBOutlet weak var showJSON: UIWebView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: urlJSoN)
+        print("url \(String(describing: url))")
+        
+        showJSON.loadRequest(URLRequest(url: url!))
+        
     }
 
 
